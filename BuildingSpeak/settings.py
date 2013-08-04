1f4756a00fb2f43c061810901c26b07f7cb21d28
David Ashley <dashley@drydenengineering.com>
2013-08-03 22:03:36 -0400

    removed BuildingSpeakApp from settings.py

--- BuildingSpeak/settings.py
+++ BuildingSpeak/settings.py
@@ -152,7 +152,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'south',
-    'BuildingSpeakApp',
+    #'BuildingSpeakApp',
     'storages',
     #'schedule',
 )
