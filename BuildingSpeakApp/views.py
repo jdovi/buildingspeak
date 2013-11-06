b868e5c773452e3edeb0bbfb849c53e77ff7fc0e
David Ashley <dashley@drydenengineering.com>
2013-11-06 00:44:01 -0500

    switching to heroku to push for checking and for backup

--- BuildingSpeakApp/views.py
+++ BuildingSpeakApp/views.py
@@ -15,8 +15,8 @@ from decimal import Decimal
 from django.forms.models import modelform_factory
 from django.contrib.auth.models import User
 from django.db.models import Q
-#from rq import Queue
-#from worker import conn
+from rq import Queue
+from worker import conn
 
 class ResultsMessage(object):
     """Used for generating user
@@ -182,9 +182,9 @@ def meter_detail(request, account_id, meter_id):
             form.initial['bill_data_file'] = latest_bill_data_file
             meter.save()
             try:
-#                q = Queue(connection=conn)
-#                result = q.enqueue(meter.upload_bill_data)
-                meter.upload_bill_data()
+                q = Queue(connection=conn)
+                result = q.enqueue(meter.upload_bill_data)
+#                meter.upload_bill_data()
                 m = ResultsMessage()
                 m.comment = 'Bill data has been uploaded.'
             except:
