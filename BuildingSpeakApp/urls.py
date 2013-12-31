0947c1bb928e09e58c8c4b94bd9b9876899132d6
David Ashley <dashley@drydenengineering.com>
2013-12-30 20:30:53 -0500

    fixed urls.py

--- BuildingSpeakApp/urls.py
+++ BuildingSpeakApp/urls.py
@@ -23,6 +23,7 @@ urlpatterns = patterns('',
     url(r'^(?P<account_id>\d+)/measures/(?P<measure_id>\d+)$', views.measure_detail,
         name='measure_detail'),
     url(r'^management/$', views.management, name='management'),
-    url(r'^tropo_test/$', views.tropo_test, name='tropo_test'),
+    url(r'^tropo_test_text/$', views.tropo_test_text, name='tropo_test_text'),
+    url(r'^tropo_test_voice/$', views.tropo_test_voice, name='tropo_test_voice'),
 
 )
