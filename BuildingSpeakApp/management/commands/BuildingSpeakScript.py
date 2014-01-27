cc5487d02398041d9d8d4da9a51c76cb62d8d60d
David Ashley <dashley@drydenengineering.com>
2014-01-26 21:55:56 -0500

    more static, mgmt commands work

--- BuildingSpeakApp/management/commands/BuildingSpeakScript.py
+++ BuildingSpeakApp/management/commands/BuildingSpeakScript.py
@@ -8,7 +8,7 @@ account = Account.objects.get(id=1)
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        file_url = STATIC_URL + 'site/temporary_files/Account Image - City of Refuge.jpg'
+        file_url = STATIC_URL + 'site/temporary_files/Account_Image_City_of_Refuge.jpg'
         file_obj = File(open(file_url))
         account.__setattr__('image_file', file_obj)
         account.save()
