99a2a1ae3c23a82473d9af73f12542298aa05498
David Ashley <dashley@drydenengineering.com>
2014-01-26 21:10:01 -0500

    testing uploading temp static files to automate loading of customer FileFields

--- BuildingSpeakApp/management/commands/BuildingSpeakScript.py
+++ BuildingSpeakApp/management/commands/BuildingSpeakScript.py
@@ -1,7 +1,14 @@
 from django.core.management.base import BaseCommand
 
+from BuildingSpeakApp.models import Account
+from BuildingSpeak.settings import STATIC_URL
+from django.core.files import File
+account = Account.objects.get(id=1)
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        pass
+        file_url = STATIC_URL + 'site/temporary_files/Account Image - City of Refuge.jpg'
+        file_obj = File(open(file_url))
+        account.__setattr__('image_file', file_obj)
+        account.save()
