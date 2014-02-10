c1d3877584ffc889d25f713b87a4a9147e191801
David Ashley <dashley@drydenengineering.com>
2014-02-10 08:25:47 -0500

    time testing account_detail page

--- BuildingSpeakApp/views.py
+++ BuildingSpeakApp/views.py
@@ -126,7 +126,6 @@ def index(request):
 
 @login_required
 def user_account(request):
-#    try:
     current_user = request.user
     if request.method == 'POST': # If the form has been submitted...
         form = UserSettingsForm(request.POST, request.FILES) # A form bound to the POST data
@@ -173,8 +172,6 @@ def user_account(request):
         'form':     form, }
     if reloading: context['alerts'] = [m]
     return render(request, 'buildingspeakapp/user_account.html', context)
-#    except:
-#        return HttpResponseRedirect('/application-error')
         
 @login_required
 def update_successful(request):
@@ -187,12 +184,15 @@ def application_error(request):
 
 @login_required
 def account_detail(request, account_id):
+    t0 = timezone.now()
     account = get_object_or_404(Account, pk=account_id)
     total_SF = account.building_set.all().aggregate(Sum('square_footage'))['square_footage__sum']
     
     account_attrs = get_model_key_value_pairs_as_nested_list(account)
     month_curr = pd.Period(timezone.now(),freq='M')-39 #current month, final month in sequence
     month_prev = month_curr - 1                     #previous month, first in sequence
+
+    t1 = timezone.now()
     
     bldg_data = []
     for bldg in account.building_set.order_by('name'):
@@ -209,20 +209,23 @@ def account_detail(request, account_id):
                           bldg_view_data_prev[1],
                           ])
     
+    t2 = timezone.now()
+
     month_first = pd.Period(timezone.now(),freq='M')-40     #first month in sequence
     month_last = month_first + 40                            #final month in sequence
     acct_view_data = account.get_account_view_meter_data(month_first=month_first,
                                                          month_last=month_last)
+    t3 = timezone.now()
     five_year_data = account.get_account_view_five_year_data()
-    print 'check4'
+    t4 = timezone.now()
     if acct_view_data is None:
         meter_data = None
         pie_data = None
     else:
         meter_data = acct_view_data[0]
         pie_data = acct_view_data[1]
-    print 'check5'
     #####Stripe testing
+    t5 = timezone.now()
     if request.method == 'POST':
         print request.POST['stripeToken']
         try:
@@ -237,6 +240,7 @@ def account_detail(request, account_id):
             account.__setattr__('stripe_customer_id', stripe_customer.id)
             account.save()
     #####Stripe testing
+    t6 = timezone.now()
     if (account.stripe_customer_id is not None) and (account.stripe_customer_id != ''):
         charge = stripe.Charge.create(
             amount = timezone.now().minute * 100,
@@ -244,7 +248,7 @@ def account_detail(request, account_id):
             customer = account.stripe_customer_id,
             description = 'Account ' + str(account.id) + ': test payment $' + str(timezone.now().minute * 100)
             )
-    print 'check6'
+    t7 = timezone.now()
     context = {
         'user':           request.user,
         'account':        account,
@@ -268,6 +272,14 @@ def account_detail(request, account_id):
         'motion_data_buildings':    account.get_account_view_motion_table_buildings(),
         'motion_data_spaces':       account.get_account_view_motion_table_spaces(),
         'stripe_pk':                stripe_pk,
+        'results_set': [['get Account, SF, attrs',      '{0:,.0f}'.format((t1-t0).seconds*1000.0 + (t1-t0).microseconds/1000.0)],
+                        ['bldg data curr/prev',         '{0:,.0f}'.format((t2-t1).seconds*1000.0 + (t2-t1).microseconds/1000.0)],
+                        ['get Account view meter data', '{0:,.0f}'.format((t3-t2).seconds*1000.0 + (t3-t2).microseconds/1000.0)],
+                        ['get Account 5yr data',        '{0:,.0f}'.format((t4-t3).seconds*1000.0 + (t4-t3).microseconds/1000.0)],
+                        ['extracting results',          '{0:,.0f}'.format((t5-t4).seconds*1000.0 + (t5-t4).microseconds/1000.0)],
+                        ['Stripe get/create customer',  '{0:,.0f}'.format((t6-t5).seconds*1000.0 + (t6-t5).microseconds/1000.0)],
+                        ['Stripe create charge',        '{0:,.0f}'.format((t7-t6).seconds*1000.0 + (t7-t6).microseconds/1000.0)],
+                        ],
     }
     user_account_IDs = [str(x.pk) for x in request.user.account_set.all()]
     if account_id in user_account_IDs:
